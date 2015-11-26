f887e2ae461d3a13842b49e94c9f3817b4043d9b
KMeshkov <meshkob@rambler.ru>
2015-11-26 22:50:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kirill/a5mobile/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.kirill.a5mobile;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btn);
+       btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "text");
+                startActivityForResult(intent, 017);
+            }
+        });
+    }
+}
